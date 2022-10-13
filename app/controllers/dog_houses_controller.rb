46bcb657b807d63929b8ef28ff1a5e3a9c898ab4
Christopher Marzulli <chriszulli15@gmail.com>
2022-10-13 13:03:32 -0400

    commit tree e5e429d22844ca36dd6863ff47796c4d8f4c7eba

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -3,7 +3,7 @@ class DogHousesController < ApplicationController
 
   def show
     dog_house = DogHouse.find(params[:id])
-    render json: dog_house
+    render json: dog_house, include: :reviews
   end
 
   private
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, include: :dog_house
   end
 
 end
